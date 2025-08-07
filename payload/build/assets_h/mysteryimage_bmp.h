45308b4d1feaca93ad6e6819657647805983b565
Finlay Christ <randomiagaming@gmail.com>
2025-08-07 03:42:29 -0700

    Removed big files for real this time.

--- payload/build/assets_h/mysteryimage_bmp.h
+++
@@ -1,6 +0,0 @@
-#ifndef mysteryimage_bmp_h
-#define mysteryimage_bmp_h
-#include <stddef.h>
-extern const size_t mysteryimage_bmp_length;
-extern const unsigned char mysteryimage_bmp_buffer[];
-#endif
